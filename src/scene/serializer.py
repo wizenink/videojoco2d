465e4760903c8d5c2834c8ddada565423bf82232
David Maseda Neira <david.maseda@udc.es>
2018-03-03 14:11:17 +0100

    Progress on Scene creation and drawing

--- src/scene/serializer.py
+++ src/scene/serializer.py
@@ -1,5 +1,14 @@
 import numpy as np
 from skimage import io as skio
+from scene import Scene
+
+CROP = (40,60,0)
+DESERT = (200,200,200)
+GRASS = (0,255,0)
+HOUSE = (0,0,0)
+ROAD = (128,128,128)
+
+TILESIZE = 40
 
 tilemap = {
     [0,0,0]       : CROP
@@ -8,9 +17,17 @@ tilemap = {
     [255,0,255]   : HOUSE
     [128,128,128] : ROAD
 }
+
 def loadLevel(levelName):
-    levelPath = resourceManager.getLevel(levelName)
-    level = skio.imread(levelPath)
-    shape = level.shape
-    for x in range(0,shape[0]):
-        for y in range(0,shape[1]):
+    levelImg = resourceManager.getLevel(levelName)
+    tilemapDict = resourceManager.getTilemap(levelMap)
+    shape = levelImg.shape
+    width = shape[0]
+    height = shape[1]
+    map = [[]]
+    for y in range(0,height):
+        row = [None] * width
+        for x in range(0,width):
+            row[x] = tilemapDict[levelImg[x,y]]
+        map.append(row)
+    level = Scene(levelName,width,height,map)
