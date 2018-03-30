a9b1b0d0861544cf5ddb8b98d1232c403ab864a8
gilberto.plaza <keko950@hotmail.com>
2018-03-30 09:19:20 +0200

    hud

--- src/scenes/level_farm.py
+++ src/scenes/level_farm.py
@@ -169,11 +169,11 @@ class Level(Scene):
 
 	def groupDraws(self,screen):
 		self.player.draw(screen,self.camera)
+		for solid in self.solids:
+			solid.draw(screen,self.camera)
 		for enemy in self.enemys:
 			enemy.draw(screen,self.camera)
 			enemy.drawUI(screen,self.camera)
-		for solid in self.solids:
-			solid.draw(screen,self.camera)
 		self.player.drawUI(screen)
 
 	def searchCollidables(self):
